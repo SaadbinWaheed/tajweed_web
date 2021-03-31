5eb4f75de944a11ad0c060f9d86877f45ec9c568
Saad Bin Waheed <saadbnwhd@live.com>
2021-04-01 01:25:09 +0500

    added arabic buttons

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div  className="container">
+          <button className="button">
+          ا   
+          </button>
+          <button className="button">
+          ب 
+          </button>
+          <button className="button">
+          ت       
+          </button>
+        </div>
       </header>
     </div>
   );
