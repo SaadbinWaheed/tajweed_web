5335763482c7c609e11307680592aedf011169a4
WareeshaZafar <wareesha8@gmail.com>
2021-04-19 02:14:48 +0500

    nn

--- src/App.js
+++ src/App.js
@@ -11,12 +11,17 @@ import ReactRoundedImage from "react-rounded-image";
 function App() {
 
  var arr = ["A","B","C","D"]
+
+ 
   return (
 
     <div className="App">
       <Navigation/> 
       <header className="App-header">
-      <ReactRoundedImage  image={quran}  imageWidth="60"  imageHeight="60"  roundedSize="5.9"/> <strong> DIGITAL QAIDA </strong>
+      <h1 style={{marginLeft: ".3in"}}/> 
+      <ReactRoundedImage  image={quran}  imageWidth="60"  imageHeight="60"  roundedSize="5.9"/>  
+      <h1 style={{marginLeft: ".2in"}}/> 
+      <strong> DIGITAL QAIDA </strong>
       </header>
      <QaidaTable/>
     </div>
