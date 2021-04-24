7131564805d6d1771b3ea45bc84b5e87dd9951ed
Saad Bin Waheed <saadbnwhd@live.com>
2021-04-24 19:10:29 +0500

    integrated new audios

--- src/components/qaidaTable.jsx
+++ src/components/qaidaTable.jsx
@@ -1,65 +1,62 @@
-import React, { useState } from "react"
+import React from "react"
 import Row from "./row";
+import {page1Links} from "../audios/page_1_links"
 
 const QaidaTable = () => {
-  const alif = "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms.mp3";
-  const ba = "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_2.mp3";
-  const ta = "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_3.mp3";
-
-  const audioLinks = [alif, ba, ta,
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_4.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_5.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_6.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_7.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_8.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_9.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_10.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_11.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_11b.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_12.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_13.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_14.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_15.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_16.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_17.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_18.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_19.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_20.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_21.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_22.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_23.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_24.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_26.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_25.mp3",
-    "",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_27.mp3",
-    "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_27.mp3",
-  ]
+  // const audioLinks = [alif, ba, ta,
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_4.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_5.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_6.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_7.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_8.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_9.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_10.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_11.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_11b.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_12.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_13.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_14.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_15.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_16.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_17.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_18.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_19.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_20.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_21.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_22.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_23.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_24.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_26.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_25.mp3",
+  //   "",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_27.mp3",
+  //   "https://quranforkids.org/wp-content/uploads/2013/04/Alphabet-Forms_27.mp3",
+  // ]
   const alphabets=["ا","ب","ت","ث","ج","ح","خ","د","ذ","ر","ز","س","ش","ص","ض","ط","ظ","ع","غ","ف","ق","ك","ل","م","ن","و","ھ","ء","ي","ے"]
 
   if (window.innerWidth > 600) {
     return (
       <div className="container">
-        <Row alphabets={alphabets.slice(0,5)} audioLinks={audioLinks.slice(0,5)}/>
-        <Row alphabets={alphabets.slice(5,10)}  audioLinks={audioLinks.slice(5,10)}/>
-        <Row alphabets={alphabets.slice(10,15)} audioLinks={audioLinks.slice(10,15)}/>
-        <Row alphabets={alphabets.slice(15,20)} audioLinks={audioLinks.slice(15,20)}/>
-        <Row alphabets={alphabets.slice(20,25)} audioLinks={audioLinks.slice(20,25)}/>
-        <Row alphabets={alphabets.slice(25,30)} audioLinks={audioLinks.slice(25,30)}/>
+        <Row alphabets={alphabets.slice(0,5)} audioLinks={page1Links.slice(0,5)}/>
+        <Row alphabets={alphabets.slice(5,10)}  audioLinks={page1Links.slice(5,10)}/>
+        <Row alphabets={alphabets.slice(10,15)} audioLinks={page1Links.slice(10,15)}/>
+        <Row alphabets={alphabets.slice(15,20)} audioLinks={page1Links.slice(15,20)}/>
+        <Row alphabets={alphabets.slice(20,25)} audioLinks={page1Links.slice(20,25)}/>
+        <Row alphabets={alphabets.slice(25,30)} audioLinks={page1Links.slice(25,30)}/>
         </div >)}
           else {
             return ( 
             <div className="container">
-            <Row alphabets={alphabets.slice(0,3)} audioLinks={audioLinks.slice(0,3)}/>
-            <Row alphabets={alphabets.slice(3,6)} audioLinks={audioLinks.slice(3,6)}/>
-            <Row alphabets={alphabets.slice(6,9)} audioLinks={audioLinks.slice(6,9)}/>
-            <Row alphabets={alphabets.slice(9,12)} audioLinks={audioLinks.slice(9,12)}/>
-            <Row alphabets={alphabets.slice(12,15)} audioLinks={audioLinks.slice(12,15)}/>
-            <Row alphabets={alphabets.slice(15,18)} audioLinks={audioLinks.slice(15,18)}/>
-            <Row alphabets={alphabets.slice(18,21)} audioLinks={audioLinks.slice(18,21)}/>
-            <Row alphabets={alphabets.slice(21,24)} audioLinks={audioLinks.slice(21,24)}/>
-            <Row alphabets={alphabets.slice(24,27)} audioLinks={audioLinks.slice(24,27)}/>
-            <Row alphabets={alphabets.slice(27,30)} audioLinks={audioLinks.slice(27,30)}/>
+            <Row alphabets={alphabets.slice(0,3)} audioLinks={page1Links.slice(0,3)}/>
+            <Row alphabets={alphabets.slice(3,6)} audioLinks={page1Links.slice(3,6)}/>
+            <Row alphabets={alphabets.slice(6,9)} audioLinks={page1Links.slice(6,9)}/>
+            <Row alphabets={alphabets.slice(9,12)} audioLinks={page1Links.slice(9,12)}/>
+            <Row alphabets={alphabets.slice(12,15)} audioLinks={page1Links.slice(12,15)}/>
+            <Row alphabets={alphabets.slice(15,18)} audioLinks={page1Links.slice(15,18)}/>
+            <Row alphabets={alphabets.slice(18,21)} audioLinks={page1Links.slice(18,21)}/>
+            <Row alphabets={alphabets.slice(21,24)} audioLinks={page1Links.slice(21,24)}/>
+            <Row alphabets={alphabets.slice(24,27)} audioLinks={page1Links.slice(24,27)}/>
+            <Row alphabets={alphabets.slice(27,30)} audioLinks={page1Links.slice(27,30)}/>
             </div >)}
         }
 /*
--- src/components/row.jsx
+++ src/components/row.jsx
@@ -5,7 +5,7 @@ const Row = (props) => {
     const buttons = props.alphabets;
     const audioLinks = props.audioLinks;
     return (
-    <div class="row justify-content-center flex-row-reverse">
+    <div className="row justify-content-center flex-row-reverse">
             {buttons.map((val,i)=><ArabicButton letter={val} isFirst={val===buttons[0]} link={audioLinks[i]}/>)}
     </div>)
 }
